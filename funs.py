a5a706719a1de75b18c605d5618994ac5e980611
rafgam10 <rafgam19@gmail.com>
2024-10-16 10:17:37 -0300

    DELETE da parte de json do arquivo data do pokémon.

--- funs.py
+++ funs.py
@@ -5,9 +5,6 @@ def requisicaoHTTPDoPokemon(nomeOuIdPokemon):
     requisicao = requests.get(f"https://pokeapi.co/api/v2/pokemon/{nomeOuIdPokemon}")
     return requisicao
 
-def jsonFile(requisicaoParaJson):
-    data = requisicaoParaJson.json()
-    return data
 
 
 
