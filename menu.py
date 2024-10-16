39bec51036b584c915a0b133256727374a16a156
rafgam10 <rafgam19@gmail.com>
2024-10-16 11:05:04 -0300

    Modificação no arquivo menu.py, utilizando o switch caseiro para escolha de exibição de dados do Pokémon.

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+from funs import *
+from main import *
+
 def exibirInfomacaoPokemon(data):
     #Tem um paramntro de data para colocar o json do pokémon;
     print("\nStatus do Pokémon.")
@@ -8,6 +11,7 @@ def exibirInfomacaoPokemon(data):
     print(f"Tipo do Pokémon: {data['types'][0]['type']['name']}")
 
 def evolucaoPokemon():
+    #Terminar está parte de evolução;
     pass
 
 def exibirMovimentosPokemon(data):
@@ -22,3 +26,32 @@ def MenuOpcao():
     print(f"3.Exibir os Movimentos do Pokémon.")
     print(f"4.Sair do Programar.")
 
+
+def switch(opcao):
+    data = None
+
+    match opcao:
+        case 1:
+            pokemonR = input("Insira o nome/id do Pokémon:")
+            r = requisicaoHTTPDoPokemon(pokemonR)
+            
+            #Verificação de conexão:
+            if r.status_code == 200:
+               data = r.json()
+
+            else:
+                print("Conexão falha!")
+                global escolha #Puxado variavel global do main.py;
+                escolha=4
+                
+        case 2:
+            exibirInfomacaoPokemon(data=data)
+
+        case 3:
+            exibirMovimentosPokemon(data=data)
+
+        case _:
+            print("Digite uma opção inválido.")
+            print("Digite novamente.")
+            valido=int(input("Digite a sua escolha:"))
+            switch(valido)
