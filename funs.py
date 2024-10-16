9d03c4f502f5cc67c63757dc476cb4b7af4102d5
rafgam10 <rafgam19@gmail.com>
2024-10-16 09:53:24 -0300

    ADD JsonFile para data.json do pokémon.

--- funs.py
+++ funs.py
@@ -5,6 +5,9 @@ def requisicaoHTTPDoPokemon(nomeOuIdPokemon):
     requisicao = requests.get(f"https://pokeapi.co/api/v2/pokemon/{nomeOuIdPokemon}")
     return requisicao
 
+def jsonFile(requisicaoParaJson):
+    data = requisicaoParaJson.json()
+    return data
 
 
 
